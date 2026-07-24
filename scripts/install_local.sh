1c61246fe6fbf62c81c08baf5239d55a9cd7e042
Caizus <18930215187@163.com>
2026-07-23 23:49:29 -0500

    test(eval): gate full tokenizer initialization
    Signed-off-by: Caizus <18930215187@163.com>

--- scripts/install_local.sh
+++ scripts/install_local.sh
@@ -472,8 +472,8 @@ backend.model_config = SimpleNamespace(
 backend._run_completion = MethodType(lambda self, **kw: captured.update(kw) or [], backend)
 settings = runpy.run_path(sys.argv[2])
 model = object.__new__(VLLMModel)
-model.config = settings["RWKVVLLMModelConfig"](
-    model_name="compatibility-probe", generation_parameters=settings["_generation_parameters"]())
+model.config = settings["RWKVVLLMModelConfig"](model_name=settings["MODEL_PATH"], generation_parameters=settings["_generation_parameters"]())
+assert ((tokenizer := model._create_auto_tokenizer(model.config)).eos_token, tokenizer.pad_token) == ("<|endoftext|>",) * 2
 model.data_parallel_size, model.model = 1, backend
 model._generate(inputs=[[1]], max_new_tokens=17, stop_tokens=[], num_samples=2)
 assert captured["prompts"] == [{"prompt_token_ids": [1]}]
